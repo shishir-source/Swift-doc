f4a1102d53f32c0df974ffa263658dd75911843f
Shishir <Shishir.Ahmed@brainstation-23.com>
2023-11-09 22:36:24 +0600

    Init

--- Basic/Basics.playground/Contents.swift
+++ Basic/Basics.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//Assignment Operator
+let b = 10
+var a = 5
+a = b
+print(a)
+let (x, y) = (1, 2)
+print(x, y)
+//Arithmetic Operatorsin page link
+/*Swift supports the four standard arithmetic operators for all number types:
+ 
+ Addition (+)
+ 
+ Subtraction (-)
+ 
+ Multiplication (*)
+ 
+ Division (/)
+ */
